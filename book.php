8c7a3b5b223f0b689bd31c1cb4e3a219f4d1e5c3
KaroliinaPilden <44017827+KaroliinaPilden@users.noreply.github.com>
2020-01-09 14:14:59 +0200

    Update book.php

--- book.php
+++ book.php
@@ -14,8 +14,21 @@ $book = $stmt->fetch();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= $book['title']; ?></title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <div class="flex">
+        <div class="inf">
+            <h1><?= $book['title']; ?></h1><br>
+            <p>Released: <?= $book['release_date']; ?></p><br>
+            <p>Language: <?= $book['language']; ?></p><br>
+            <p>Pages: <?= $book['pages']; ?></p><br>
+            <p>Price: <?= round($book['price'], 2); ?>€</p><br>
+            <p>Available now: <?= $book['stock_saldo']; ?></p><br>
+            <p>Type: <?= $book['type']; ?></p><br>
+            <p>Summary: <br> <?= $book['summary']; ?></p><br>
+        </div>
+        <img src="<?= $book['cover_path']; ?>" alt="" style="align-self: flex-start">
+    </div>
 </body>
 </html>
